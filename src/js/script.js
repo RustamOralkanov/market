5e72356682d3c2c9040ccdb39697a8584dbfd85b
Rustam <oralkanov1r@icloud.com>
2022-09-13 12:55:49 +0600

    mobile adapt

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,37 @@
 var swiper = new Swiper(".main-slide", {
     slidesPerView: 2,
     spaceBetween: 30,
+    breakpoints: {
+    320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+    },
+    769: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+    }
+    }
 });
 
 var swiper = new Swiper(".smcard", {
     slidesPerView: 4,
     spaceBetween: 20,
     breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        676: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
         1200: {
-          slidesPerView: 3,
-          spaceBetween: 20,
+            slidesPerView: 3,
+            spaceBetween: 20,
         },
         1500: {
             slidesPerView: 4,
@@ -21,16 +43,70 @@ var swiper = new Swiper(".smcard", {
 var swiper = new Swiper(".brands-slider", {
     slidesPerView: 'auto',
     spaceBetween: 20,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        360: {
+            slidesPerView: 'auto',
+            spaceBetween: 10,
+        }
+    }
 });
 
 var swiper = new Swiper(".discount-slider", {
     slidesPerView: 'auto',
     spaceBetween: 20,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        360: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        577: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        769: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+        },
+        992: {
+            slidesPerView: "auto",
+            spaceBetween: 10,
+        },
+    }
 });
 
 var swiper = new Swiper(".discount-slider-catalog", {
     slidesPerView: 'auto',
     spaceBetween: 20,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        360: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        577: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        769: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+        },
+        992: {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+        },
+    }
 });
 
 var swiper = new Swiper(".item-slider", {
@@ -39,23 +115,65 @@ var swiper = new Swiper(".item-slider", {
     spaceBetween: 16,
 });
 
-var swiper2 = new Swiper(".item-slider-2", {
+var swiper = new Swiper(".item-slider-2", {
     slidesPerView: 1,
-    allowTouchMove: false,
+    allowTouchMove: true,
     effect: "fade",
     thumbs: {
-      swiper: swiper,
+        swiper: swiper,
     },
+    breakpoints: {
+        320: {
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        },
+        993: {
+            pagination: false
+        },
+    } 
 });
 
-let skills = {
-    a: 'карате',
-    b: 'бокс',
-    c: 'c'
-};
+window.addEventListener('load', () => { 
+  const preloader = document.querySelector('.preloader');
+  preloader.classList.add('preloader_hidden');
+});
 
+const hamburger = document.querySelector('.header-mobile-hamburger');
+const topMenu = document.querySelector('#topMenu');
 
-for (let key in skills) {
-    console.log(key);
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('header-mobile-hamburger-active');
+
+    if (hamburger.classList.contains('header-mobile-hamburger-active')) {
+        topMenu.style.top = '65px';
+        document.querySelector('body').style.overflow = 'hidden';
+    } else {
+        topMenu.style.top = '';
+        document.querySelector('body').style.overflow = '';
+    }
+});
+
+const filter = document.querySelector('#filter');
+const filterBlock = document.querySelector('.products-filter');
+const overlay = document.createElement('div');
+
+function filterToggle() {
+
+    filterBlock.classList.toggle('products-filter-active');
+
+    if (filterBlock.classList.contains('products-filter-active')) {
+
+        document.querySelector('body').style.overflow = 'hidden';
+        overlay.classList.add('overlay');
+        document.querySelector('body').append(overlay);
+
+    } else {
+        document.querySelector('body').style.overflow = '';
+        overlay.remove();
+    }
 }
 
+filter.addEventListener('click', filterToggle);
+
+overlay.addEventListener('click', filterToggle);
